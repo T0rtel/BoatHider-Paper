f2acd5148e2d1279b6af67186d92c2a23c6addfc
Tortel <rexdel713@gmail.com>
2026-04-26 21:38:20 +0300

    init commit

---
+++ src/main/java/me/tortel/boatHider1218/Main.java
@@ -0,0 +1,19 @@
+package me.tortel.boatHider1218;
+
+import org.bukkit.craftbukkit.entity.CraftBoat;
+import org.bukkit.entity.Boat;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/tortel/boatHider1218/commands/devCommands.java
@@ -0,0 +1,20 @@
+package me.tortel.boatHider1218.commands;
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
+import co.aikar.commands.annotation.Subcommand;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+@CommandAlias("dev")
+public class devCommands extends BaseCommand {
+
+    @Subcommand("doTask")
+    public void doTask(CommandSender sender) {
+
+
+    }
+
+
+}
